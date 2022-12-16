4c3c02ee88bd564e909afb88eb0f18945d18729c
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-12-16 23:35:55 +0530

    Emp Part Time

---
+++ EmpPartTime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewageproblem
+{
+    public class EmployeeCheck
+    {
+
+
+        public static void CheckAttendance()
+
+        {
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int empHrs = 0;
+            int EMP_RATE_PER_HOUR = 20;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+
+                empHrs = 0;
+            }
+            else
+            {
+
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage : " + empWage);
+        }
+    }
+}
