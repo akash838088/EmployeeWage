b3ab912e3d50e6f55427febe777e1490b6064dce
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-12-16 20:48:34 +0530

    Greeting Message

---
+++ EmployeeCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewageproblem
+{
+    public class EmployeeCheck
+    {
+        public static void CheckAttendance()
+
+        {
+            int IS_FULL_TIME = 1;
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+        }
+    }
+}
